9ff61d8aa91af7a9971c41afa52b6de05016d778
Rafael Carlos De Souza <rf2014souza@gmail.com>
2022-01-25 08:48:57 -0300

    :package: Corrigindo

--- restfull/server.js
+++ restfull/server.js
@@ -1,15 +1,16 @@
 import express, { urlencoded, json } from 'express';
-import consign from 'consign';
+//import consign from 'consign';
 import expressValidator from 'express-validator';
 
 const app = express();
 
 app.use(urlencoded({ extended: false }));
+app.set("rotas")
 app.use(json());
 app.use(expressValidator());
 
 
-consign().include('rotas').include('utius').into(app)//pasta rotas
+//consign().include('rotas').include('utius').into(app)//pasta rotas
 
 app.listen(4000, () => {
     console.log('Servidor rodando na porta 4000...')
